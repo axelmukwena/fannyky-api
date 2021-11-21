262b41f256f7e32ae8625f35ea96c0dd4c59fd56
Axel Mukwena <axel.muk@gmail.com>
2021-11-21 16:56:10 +0800

    Update award.rb

--- app/models/award.rb
+++ app/models/award.rb
@@ -2,7 +2,7 @@ class Award < ApplicationRecord
   belongs_to :painter, counter_cache: :awards_count
   belongs_to :user
   has_many_attached :images
-  paginates_per 50
+  paginates_per 60
 
   extend FriendlyId
   friendly_id :title, use: :slugged
