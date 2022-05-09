be28557853e72a0062b3664df1965f3fd86e2374
Axel Mukwena <axel.muk@gmail.com>
2022-05-10 05:51:55 +0800

    Update painter_serializer.rb

--- app/serializers/painter_serializer.rb
+++ app/serializers/painter_serializer.rb
@@ -4,6 +4,11 @@ class PainterSerializer < ActiveModel::Serializer
              :exhibitions_count, :talks_count, :pagelink, :menuitems,
              :awards_count, :user, :paintings_categories, :images
 
+  def paintings_categories
+    return unless object.paintings_categories.blank?
+    JSON.parse(object.paintings_categories)
+  end
+
   def images
     return unless object.images.attachments
     object.images.map do |image|
